c8c390f567f116251b1ef7986183bc7d7e15a688
felipearaujodev <43017434+felipearaujodev@users.noreply.github.com>
2022-09-22 22:20:09 -0300

    criando menu

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+
+    
+<template>
+  <div id="nav">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/processos">Processos</RouterLink>
+  </div>
+</template>
+
+
+
+<style scoped>
+  #nav {
+    background-color: black;
+    border-bottom: 4px solid #000;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  #nav a {
+    color:coral;
+    text-decoration: none;
+    margin: 17px;
+    transition: .10s;
+  }
+  #nav a:hover {
+    color:aliceblue
+  }
+</style>
+
+<script>
+
+  export default {
+    name: "Navbar"
+  }
+</script>
+    
--- src/views/ProcessosView.vue
+++ src/views/ProcessosView.vue
@@ -1,16 +1,9 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="main-container">
-
-    <h1>Bem vindo!</h1>
-
+    <h1>PROCESSOS</h1>
   </div>
 </template>
 
-
 <style>
 
-  </style>
+</style>
